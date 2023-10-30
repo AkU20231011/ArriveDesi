212669f12e9d2828ffb3e4ab5d02380e95068114
Abhi <rds.abhisheku@gmail.com>
2023-10-30 14:08:17 +0530

    Initial commit

---
+++ script/new-script.js
@@ -0,0 +1,66 @@
+jQuery(function($){
+    $('.slider-con').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite:false,
+    autoplay: false,
+    autoplaySpeed: 700,
+    arrows: false,
+    dots: true,
+    pauseOnHover: true,
+    adaptiveHeight: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+    breakpoint: 980,
+    settings: {
+    slidesToShow: 4.5,
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 2,
+    }
+    }
+    ]
+    });
+    });
+jQuery(function($){
+    $('.vid-slider-con').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite:false,
+    autoplay: false,
+    autoplaySpeed: 700,
+    arrows: true,
+    dots: false,
+    pauseOnHover: true,
+    adaptiveHeight: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+    breakpoint: 980,
+    settings: {
+    slidesToShow: 1,
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+    }
+    }
+    ]
+    });
+    });
+
+
+
+// const elements = document.querySelectorAll('.slider_vid');
+
+// elements.forEach(element => {
+//   const width = element.offsetWidth;
+//   element.style.height = `${width}px`; // Set the height equal to the width
+//   console.log(`Height of element: ${width}px`);
+// });
